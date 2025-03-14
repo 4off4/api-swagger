9ea1d19eaae218ee6cf70c7b52b77d3ef4052070
eunhee4-thetak <eunhee4-thetak@gmail.com>
2025-03-14 17:52:03 +0900

    Initial commit

---
+++ src/test/java/project/demo/apiswagger/ApiSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.demo.apiswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
